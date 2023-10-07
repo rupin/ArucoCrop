871783e7ec1a6435da27dd9abab6dfb63266a750
Rupin <rupin@whitehatjr.com>
2023-10-07 15:46:24 +0530

    AWB

--- testPiCamera.py
+++ testPiCamera.py
@@ -2,10 +2,10 @@ from picamera import PiCamera
 from time import sleep
 camera = PiCamera()
 camera.resolution = (640, 480)
-camera.brightness = 50
+camera.brightness = 70
 camera.contrast = 70
-camera.saturation = 50
-camera.awb_mode = 'incandescent'
+camera.saturation = 10
+camera.awb_mode = 'auto'
 camera.start_preview()
 sleep(5)
 camera.capture('click.jpg')
