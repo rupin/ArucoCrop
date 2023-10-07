40e82b7884690fc79b143de6ab3a6168986cee90
Rupin <rupin@whitehatjr.com>
2023-10-07 15:47:35 +0530

    AWB

--- pi-main.py
+++ pi-main.py
@@ -13,8 +13,8 @@ camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 camera.brightness = 70
 camera.contrast = 70
-camera.saturation = 0
-camera.awb_mode = 'incandescent'
+camera.saturation = 10
+camera.awb_mode = 'auto'
 
 # Create an OpenCV VideoCapture object
 cap = cv2.VideoCapture()
